fc95d36bfe714c3ed969d3cfcbd47f1b7624a804
Sylvan Smit <sylvan@sylvansmit.com>
2025-07-11 17:56:46 +0200

    Enable which-key view
    Signed-off-by: Sylvan Smit <sylvan@sylvansmit.com>

--- init.lua
+++ init.lua
@@ -659,12 +659,20 @@ require('lazy').setup({
           F12 = '<F12>',
         },
       },
-
       -- Document existing key chains
       spec = {
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      },
+    },
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show { global = false }
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
       },
     },
   },
